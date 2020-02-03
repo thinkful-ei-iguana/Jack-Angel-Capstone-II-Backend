9585e8c44808e4b9ee7560ea35e1d407e8349f7d
Angel Alicea <ala31996@gmail.com>
2020-02-03 13:18:59 -0500

    added language

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Spanish', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'Qué', 'What', 2),
+  (2, 1, 'Quién', 'Who', 3),
+  (3, 1, 'Dónde', 'Where', 4),
+  (4, 1, 'Por qué', 'Why', 5),
+  (5, 1, 'Cuál', 'Which', 6),
+  (6, 1, 'Cómo', 'How', 7),
+  (7, 1, 'Adiós', 'Goodbye', 8),
+  (8, 1, 'Nunca ', 'Never', 9),
+  (9, 1, 'Claro', 'Of course', 10),
+  (10, 1,'Siempre', 'Always', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
